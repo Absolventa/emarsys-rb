ac1e292327f56cd3d95ea58f96db0f64296a38ea
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2013-07-26 10:13:18 +0200

    static data object emails_status_code

---
+++ lib/emarsys/data_objects/email_status_code.rb
@@ -0,0 +1,21 @@
+module Emarsys
+  class EmailStatusCode < DataObject
+    CODES = [
+      {'1'   => 'In design'},
+      {'2'   => 'Tested'},
+      {'3'   => 'Launched'},
+      {'4'   => 'Ready to launch'},
+      {'-3'  => 'Deactivated'}
+    ]
+
+    class << self
+      def collection
+        CODES
+      end
+
+      def resource(id)
+        CODES.select{|hash| hash.has_key?(id.to_s)}[0]
+      end
+    end
+  end
+end
