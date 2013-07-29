8aa3bc7058a43ffdeba3de535d785573e87d807e
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2013-07-29 12:40:51 +0200

    spec params converter

---
+++ spec/emarsys/params_converter_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe Emarsys::ParamsConverter do
+  let(:converter) { Emarsys::ParamsConverter.new({}) }
+
+  before :each do
+    Emarsys::FieldMapping.send(:remove_const, 'ATTRIBUTES')
+    Emarsys::FieldMapping::ATTRIBUTES = [
+      {:id => 0,   :identifier => 'interests', :name => 'Interests'},
+      {:id => 1,   :identifier => 'first_name', :name => 'First Name'},
+      {:id => 2,   :identifier => 'last_name',  :name => 'Last Name'},
+    ]
+  end
+
+  describe 'with attributes' do
+    it 'should set params attribute on initialize' do
+      params = {1 => 'Jane', 2 => 'Doe'}
+      params_converter = Emarsys::ParamsConverter.new(params)
+      params_converter.params.should eq(params)
+    end
+  end
+
+  context 'converting id-based hashes into string keys' do
+    it 'converts all findable string keys' do
+      converter.params = {'first_name' => 'Jane', 'last_name' => 'Doe'}
+      expect(converter.convert_to_ids).to eq({1 => 'Jane', 2 => 'Doe'})
+    end
+
+    it 'does not convert non-findable keys' do
+      converter.params = {'first_name' => 'Jane', 'last_name' => 'Doe', 'something' => 'abc'}
+      expect(converter.convert_to_ids).to eq({1 => 'Jane', 2 => 'Doe', 'something' => 'abc'})
+    end
+  end
+
+  context 'converting string-based hashes into id keys' do
+    it 'converts all findable keys' do
+      converter.params = {1 => 'Jane', 2 => 'Doe'}
+      expect(converter.convert_to_identifiers).to eq({'first_name' => 'Jane', 'last_name' => 'Doe'})
+    end
+
+    it 'does not convert 0-key (since ''.to_i, which is 0, is a relevant field at emarsys)' do
+      converter.params = {0 => 'Yeah', 1 => 'Jane', 2 => 'Doe'}
+      expect(converter.convert_to_identifiers).to eq({0 => 'Yeah', 'first_name' => 'Jane', 'last_name' => 'Doe'})
+    end
+
+    it 'does not convert non-findable keys' do
+      converter.params = {1 => 'Jane', 2 => 'Doe', 4 => 'something'}
+      expect(converter.convert_to_identifiers).to eq({'first_name' => 'Jane', 'last_name' => 'Doe', 4 => 'something'})
+    end
+  end
+
+end
