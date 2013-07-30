93def80fd6d0e64a0886a0b5d0abbeb08be31f75
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2013-07-30 09:38:27 +0200

    files can be filtered by folder

--- spec/emarsys/data_objects/file_spec.rb
+++ spec/emarsys/data_objects/file_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe Emarsys::File do
   describe ".collection" do
-    it "requests all languages" do
+    it "requests all files" do
       stub_get("file") { Emarsys::File.collection }.should have_been_requested.once
+    end
+
+    it "requests all files with parameter" do
+      stub_get("file/folder=3") { Emarsys::File.collection(:folder => 3) }.should have_been_requested.once
     end
   end
 end
