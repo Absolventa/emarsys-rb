f472c3b8c4d1b787390b668d63a3e715dfaee47b
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2013-07-31 06:26:42 +0200

    implement file creation

--- lib/emarsys/data_objects/file.rb
+++ lib/emarsys/data_objects/file.rb
@@ -4,6 +4,12 @@ module Emarsys
       def collection(params = {})
         get 'file', params
       end
+
+      def create(filename, base64_encoded_file, folder_id = nil)
+        params = {:filename => filename, :file => base64_encoded_file}
+        params.merge!({:folder => folder_id}) unless folder_id.nil?
+        post 'file', params
+      end
     end
   end
 end
--- spec/emarsys/data_objects/file_spec.rb
+++ spec/emarsys/data_objects/file_spec.rb
@@ -10,4 +10,20 @@ describe Emarsys::File do
       stub_get("file/folder=3") { Emarsys::File.collection(:folder => 3) }.should have_been_requested.once
     end
   end
+
+  describe ".create" do
+    it "requests file creation with parameters" do
+      stub_params = {:filename => 'my_file.jpg', :file => 'base_64_encoded_string'}
+      stub = stub_request(:post, "https://suite5.emarsys.net/api/v2/file").with(:body => stub_params.to_json).to_return(standard_return_body)
+      Emarsys::File.create('my_file.jpg', 'base_64_encoded_string')
+      stub.should have_been_requested.once
+    end
+
+    it "requests file creation with optional folder parameter" do
+      stub_params = {:filename => 'my_file.jpg', :file => 'base_64_encoded_string', :folder => 3}
+      stub = stub_request(:post, "https://suite5.emarsys.net/api/v2/file").with(:body => stub_params.to_json).to_return(standard_return_body)
+      Emarsys::File.create('my_file.jpg', 'base_64_encoded_string', 3)
+      stub.should have_been_requested.once
+    end
+  end
 end
