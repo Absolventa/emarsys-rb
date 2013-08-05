0e99836b241b8103624115c3d7b49b48fc51fd40
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2013-08-05 15:21:59 +0200

    use required params for updating and creating contacts

--- lib/emarsys/data_objects/contact.rb
+++ lib/emarsys/data_objects/contact.rb
@@ -1,13 +1,24 @@
 module Emarsys
   class Contact < DataObject
     class << self
-      def create(params = {})
-        post "contact", params
+      def create(key_id, key_value, params = {})
+        post "contact", params.merge!({'key_id' => transform_key_id(key_id), transform_key_id(key_id) => key_value})
       end
 
-      def update(params = {})
-        put "contact", params
+      def update(key_id, key_value, params = {})
+        put "contact", params.merge!({'key_id' => transform_key_id(key_id), transform_key_id(key_id) => key_value})
       end
+
+      # Fake destroy - delete all attributes
+      def destroy(key_id, key_value, params = {})
+        update(key_id, key_value, params)
+      end
+
+      def transform_key_id(key_id)
+        matching_attributes = Emarsys::FieldMapping::ATTRIBUTES.find{|elem| elem[:identifier] == key_id.to_s}
+        matching_attributes.nil? ? key_id : matching_attributes[:id]
+      end
+
     end
   end
 end
--- spec/emarsys/data_objects/contact_spec.rb
+++ spec/emarsys/data_objects/contact_spec.rb
@@ -2,25 +2,19 @@ require 'spec_helper'
 
 describe Emarsys::Contact do
   describe ".create" do
-    it "requests contact creation" do
-      stub = stub_request(:post, "https://suite5.emarsys.net/api/v2/contact").to_return(standard_return_body)
-      Emarsys::Contact.create
-      stub.should have_been_requested.once
-    end
-
     it "requests contact creation with parameters" do
-      stub_params = {1 => 'John', 2 => "Doe", 3 => "john.doe@example.com"}
-      stub = stub_request(:post, "https://suite5.emarsys.net/api/v2/contact").with(:body => stub_params.to_json).to_return(standard_return_body)
-      Emarsys::Contact.create(stub_params)
+      stub_params = {1 => 'John', 2 => "Doe"}
+      stub = stub_request(:post, "https://suite5.emarsys.net/api/v2/contact").with(:body => stub_params.merge!({'key_id' => 3, 3 => 'john.doe@example.com'}).to_json).to_return(standard_return_body)
+      Emarsys::Contact.create(3, "john.doe@example.com", stub_params)
       stub.should have_been_requested.once
     end
   end
 
   describe ".update" do
     it "requests contact update with parameters" do
-      stub_params = {1 => 'Jane', 2 => "Doe", 3 => "jane.doe@example.com"}
-      stub = stub_request(:put, "https://suite5.emarsys.net/api/v2/contact").with(:body => stub_params.to_json).to_return(standard_return_body)
-      Emarsys::Contact.update(stub_params)
+      stub_params = {1 => 'Jane', 2 => "Doe"}
+      stub = stub_request(:put, "https://suite5.emarsys.net/api/v2/contact").with(:body => stub_params.merge!({'key_id' => 3, 3 => 'jane.doe@example.com'}).to_json).to_return(standard_return_body)
+      Emarsys::Contact.update(3, 'jane.doe@example.com', stub_params)
       stub.should have_been_requested.once
     end
   end
