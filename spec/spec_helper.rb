0a8b943b9714d045b61942e2f293922a231dc510
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2013-08-06 14:30:55 +0200

    bump up version - without launching. Crazy.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'emarsys'
 require 'rspec'
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(:allow => 'coveralls.io')
+WebMock.disable_net_connect!
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
