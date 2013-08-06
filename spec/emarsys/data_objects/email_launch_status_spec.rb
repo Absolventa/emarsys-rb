cf275891eaa24209894c60121a27e05b6bf9b7d6
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2013-08-06 15:31:56 +0200

    fix pending specs

--- spec/emarsys/data_objects/email_launch_status_spec.rb
+++ spec/emarsys/data_objects/email_launch_status_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 
 describe Emarsys::EmailLaunchStatus do
+  describe "CODES constant" do
+    it "is an array of size 4" do
+      expect(Emarsys::EmailLaunchStatus::CODES.size).to eq(4)
+    end
+  end
+
   describe ".collection" do
     it "requests all email_launch_status" do
-      pending
+      expect(Emarsys::EmailLaunchStatus.collection).to eq(Emarsys::EmailLaunchStatus::CODES)
     end
   end
 
   describe ".resource" do
     it "requests a single email launch status" do
-      pending
+      expect(Emarsys::EmailLaunchStatus.resource(0)).to eq({'0'   => 'Not launched'})
     end
 
     it "returns nil if it cannot find a launch code" do
-      pending
+      expect(Emarsys::EmailLaunchStatus.resource(100)).to eq(nil)
     end
   end
 end
--- spec/emarsys/data_objects/email_status_code_spec.rb
+++ spec/emarsys/data_objects/email_status_code_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 
 describe Emarsys::EmailStatusCode do
+  describe "CODES constant" do
+    it "is an array of size 5" do
+      expect(Emarsys::EmailStatusCode::CODES.size).to eq(5)
+    end
+  end
+
   describe ".collection" do
     it "requests all email_status_codes" do
-      pending
+      expect(Emarsys::EmailStatusCode.collection).to eq(Emarsys::EmailStatusCode::CODES)
     end
   end
 
   describe ".resource" do
     it "requests a single email status code hash" do
-      pending
+      expect(Emarsys::EmailStatusCode.resource(1)).to eq({'1'   => 'In design'})
     end
 
     it "returns nil if it cannot find a status code" do
-      pending
+      expect(Emarsys::EmailStatusCode.resource(100)).to eq(nil)
     end
   end
 end
