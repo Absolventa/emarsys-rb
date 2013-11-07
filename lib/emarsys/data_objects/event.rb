098bd36747d5824ae3ac12e6a36ae54664e15b7c
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2013-11-07 11:36:58 +0100

    Enable event to be triggered with additional data parameters

--- lib/emarsys/data_objects/event.rb
+++ lib/emarsys/data_objects/event.rb
@@ -21,11 +21,22 @@ module Emarsys
       # @param event_id [Integer, String] The internal emarsys id
       # @param key_id [Integer, String] The identifer of the key field (e.g. 3 for 'email')
       # @param external_id [String] The id of the given filed specified with key_id (e.g. 'test@example.com')
+      # @option data [Hash] data hash for transactional mails
       # @return [Hash] Result data
       # @example
-      #   Emarsys::Export.trigger(2, 3, 'test@example.com')
-      def trigger(event_id, key_id, external_id)
-        post "event/#{event_id}/trigger", {:key_id => key_id, :external_id => external_id}
+      #   Emarsys::Event.trigger(2, 3, 'test@example.com')
+      #   Emarsys::Event.trigger(2, 'user_id', 57687, {:global => {:name => "Special Name"}})
+      def trigger(event_id, key_id, external_id, data = {})
+        transformed_key_id = transform_key_id(key_id)
+        params = {:key_id => transformed_key_id, :external_id => external_id}
+        params.merge!(:data => data) if not data.empty?
+        post "event/#{event_id}/trigger", params
+      end
+
+      # @private
+      def transform_key_id(key_id)
+        matching_attributes = Emarsys::FieldMapping::ATTRIBUTES.find{|elem| elem[:identifier] == key_id.to_s}
+        matching_attributes.nil? ? key_id : matching_attributes[:id]
       end
     end
 
--- spec/emarsys/data_objects/event_spec.rb
+++ spec/emarsys/data_objects/event_spec.rb
@@ -13,5 +13,11 @@ describe Emarsys::Event do
       Emarsys::Event.trigger(123, 3, 'jane.doe@example.com')
       stub.should have_been_requested.once
     end
+
+    it "requests event trigger with additional data parameters" do
+      stub = stub_request(:post, "https://suite5.emarsys.net/api/v2/event/123/trigger").with(:body => {'key_id' => 3, 'external_id' => "jane.doe@example.com", :data => {'global' => {'my_placeholder' => 'Something'}}}.to_json).to_return(standard_return_body)
+      Emarsys::Event.trigger(123, 3, 'jane.doe@example.com', {'global' => {'my_placeholder' => 'Something'}})
+      stub.should have_been_requested.once
+    end
   end
 end
