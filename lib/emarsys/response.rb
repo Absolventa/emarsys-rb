a93a6ee78a747d95cdd1a7cb6b4a9f75d6d76dcc
Michael Sauter <michael.sauter@experteer.com>
2017-03-21 09:40:02 +0100

    Return response, not just (parsed) body of it
    This allows to inspect headers etc in the future.

--- lib/emarsys/response.rb
+++ lib/emarsys/response.rb
@@ -13,20 +13,18 @@ module Emarsys
         self.text = json['replyText']
         self.data = json['data']
       end
-      self.status = response.code if response.respond_to?(:code)
-    end
 
-    def result
-      if code == 0
-        data
-      elsif !status.nil? && status == 401
-        raise Emarsys::Unauthorized.new(code, text, status)
-      elsif !status.nil? && status == 429
-        raise Emarsys::TooManyRequests.new(code, text, status)
-      else
-        raise Emarsys::BadRequest.new(code, text, status)
+      self.status = response.code if response.respond_to?(:code)
+
+      if code != 0
+        if status == 401
+          raise Emarsys::Unauthorized.new(code, text, status)
+        elsif status == 429
+          raise Emarsys::TooManyRequests.new(code, text, status)
+        else
+          raise Emarsys::BadRequest.new(code, text, status)
+        end
       end
     end
-
   end
 end
