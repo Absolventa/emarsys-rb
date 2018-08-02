775833ad54b0bfbee307d4a98ea1983f5ec59be6
Michal Dudzinski <michal@dudzin.ski>
2018-08-02 15:27:58 +0200

    Add 'contacts' support to Segment data object
    Implements https://dev.emarsys.com/v2/segments/list-contacts-in-a-segment

--- lib/emarsys/data_objects/segment.rb
+++ lib/emarsys/data_objects/segment.rb
@@ -16,6 +16,19 @@ module Emarsys
         get account, 'filter', {}
       end
 
+      # List ids of contacts in a segment.
+      # This may return HTTP 202 {"replyCode":10001,"replyText":"The segment is currently evaluated.","data":""}
+      # if the segment is being currently processed.
+      # Reference: https://dev.emarsys.com/v2/response-codes/http-202-errors
+      # The `limit` param is required by this endpoint although it is marked as optional in the API v2 doc.
+      #
+      # @param id [Integer] the id of the segment
+      # @param limit [Integer] the maximum number of records to return
+      # @param offset [Integer] optional offset value for pagination
+      def contacts(id, limit:, offset: 0, account: nil)
+        path = "filter/#{id}/contacts"
+        get account, path, limit: limit, offset: offset
+      end
     end
   end
 
--- spec/emarsys/data_objects/segment_spec.rb
+++ spec/emarsys/data_objects/segment_spec.rb
@@ -7,5 +7,12 @@ describe Emarsys::Segment do
         stub_get("filter") { Emarsys::Segment.collection }
       ).to have_been_requested.once
     end
+    describe ".contacts" do
+      it "requests contact data for a given segment" do
+        stub = stub_request(:get, "https://api.emarsys.net/api/v2/filter/123/contacts/?limit=2000&offset=0").to_return(standard_return_body)
+        Emarsys::Segment.contacts(123, limit: 2000)
+        expect(stub).to have_been_requested.once
+      end
+    end
   end
 end
