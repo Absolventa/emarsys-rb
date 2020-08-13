903a8fc69fa3c18198857eb6402427d76553d138
Michal Dudzinski <michal@dudzin.ski>
2020-08-13 17:25:43 +0200

    Add Segment Run endpoints for multiple contacts
    Implements:
    * https://dev.emarsys.com/v2/segments/run-a-contact-segment-batch
    * https://dev.emarsys.com/v2/segments/poll-the-status-of-a-segment-run-for-multiple-contacts

--- lib/emarsys/data_objects/segment.rb
+++ lib/emarsys/data_objects/segment.rb
@@ -28,6 +28,24 @@ module Emarsys
         path = "filter/#{id}/contacts"
         get account, path, limit: limit, offset: offset
       end
+
+      # Run a Segment for Multiple Contacts
+      # Reference: https://dev.emarsys.com/v2/segments/run-a-contact-segment-batch
+      #
+      # @param id [Integer] the id of the segment
+      def run(id, account: nil)
+        path = "filter/#{id}/runs"
+        post account, path, {}
+      end
+
+      # Poll the Status of a Segment Run for Multiple Contacts
+      # Reference: https://dev.emarsys.com/v2/segments/poll-the-status-of-a-segment-run-for-multiple-contacts
+      #
+      # @param run_id [Integer] the id of the segment run, @see #run
+      def status(run_id, account: nil)
+        path = "filter/runs/#{run_id}"
+        get account, path, {}
+      end
     end
   end
 
--- spec/emarsys/data_objects/segment_spec.rb
+++ spec/emarsys/data_objects/segment_spec.rb
@@ -14,5 +14,19 @@ describe Emarsys::Segment do
         expect(stub).to have_been_requested.once
       end
     end
+    describe ".run" do
+      it "requests a run for a segment for multiple contacts" do
+        expect(
+          stub_post("filter/123/runs") { Emarsys::Segment.run(123) }
+        ).to have_been_requested.once
+      end
+    end
+    describe ".status" do
+      it "requests the status of the given segment run" do
+        expect(
+          stub_get("filter/runs/333") { Emarsys::Segment.status(333) }
+        ).to have_been_requested.once
+      end
+    end
   end
 end
